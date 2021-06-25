58c207ee0d521245217daa0594fd5a2ab2ab7c99
Pedro Gaspar <pedro.mig.gaspar@gmail.com>
2021-06-25 10:05:53 +0100

    Lists, head and tail

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -57,6 +57,10 @@ defmodule Servy.Handler do
     |> handle_file(conv)
   end
 
+  def route(%Conv{method: "POST", path: "/bears", params: params} = conv) do
+    %{conv | status: 201, resp_body: "Create a #{params["type"]} bear named #{params["name"]}!"}
+  end
+
   def route(%Conv{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
   end
@@ -198,6 +202,21 @@ Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+POST /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 21
+
+name=Baloo&type=Brown
 """
 
 response = Servy.Handler.handle(request)
