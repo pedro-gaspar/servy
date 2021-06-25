fcd79c39decea4dadb63bd9f98ac2ff8b183e453
Pedro Gaspar <pedro.mig.gaspar@gmail.com>
2021-06-25 08:20:26 +0100

    Organize code exercise

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -3,6 +3,7 @@ defmodule Servy.Handler do
 
   import Servy.Plugins, only: [track: 1, log: 1, rewrite_path: 1]
   import Servy.Parser, only: [parse: 1]
+  import Servy.FileHandler, only: [handle_file: 2]
 
   @pages_path Path.expand("../../pages", __DIR__)
 
@@ -71,18 +72,6 @@ defmodule Servy.Handler do
 
   def route(%{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
-  end
-
-  def handle_file({:ok, content}, conv) do
-    %{conv | status: 200, resp_body: content}
-  end
-
-  def handle_file({:error, :enoent}, conv) do
-    %{conv | status: 404, resp_body: "File not found!"}
-  end
-
-  def handle_file({:error, reason}, conv) do
-    %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
   def format_response(conv) do
