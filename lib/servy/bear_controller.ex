a75a77b27328ee82a23b4a208b2a33b8f82a0fd5
Pedro Gaspar <pedro.mig.gaspar@gmail.com>
2021-06-26 08:24:29 +0100

    Using compiled views

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,17 +1,9 @@
 defmodule Servy.BearController do
   alias Servy.Bear
+  alias Servy.BearView
   alias Servy.Wildthings
 
   @templates_path Path.expand("templates", File.cwd!())
-
-  defp render(conv, template, bindings \\ []) do
-    content =
-      @templates_path
-      |> Path.join(template)
-      |> EEx.eval_file(bindings)
-
-    %{conv | status: 200, resp_body: content}
-  end
 
   def index(conv) do
     bears =
@@ -19,13 +11,13 @@ defmodule Servy.BearController do
       |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
 
-    render(conv, "index.eex", bears: bears)
+    %{conv | status: 200, resp_body: BearView.index(bears)}
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    render(conv, "show.eex", bear: bear)
+    %{conv | status: 200, resp_body: BearView.show(bear)}
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
