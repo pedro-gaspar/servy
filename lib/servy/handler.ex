1aae25952f13e2d66b4aa91338fad6f0b058daa9
Pedro Gaspar <pedro.mig.gaspar@gmail.com>
2021-07-01 10:42:30 +0100

    Use task

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -7,7 +7,6 @@ defmodule Servy.Handler do
   alias Servy.Conv
   alias Servy.BearController
   alias Servy.VideoCam
-  alias Servy.Fetcher
 
   @pages_path Path.expand("pages", File.cwd!())
 
@@ -24,14 +23,14 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/sensors"} = conv) do
-    pid4 = Fetcher.async(fn -> Servy.Tracker.get_location("bigfoot") end)
+    task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
 
     snapshots =
       ["cam-1", "cam-2", "cam-3"]
-      |> Enum.map(&VideoCam.get_snapshot/1)
-      |> Enum.map(&Fetcher.get_result/1)
+      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
+      |> Enum.map(&Task.await/1)
 
-    where_is_bigfoot = Fetcher.get_result(pid4)
+    where_is_bigfoot = Task.await(task)
 
     %{conv | status: 200, resp_body: inspect({snapshots, where_is_bigfoot})}
   end
